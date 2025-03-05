7074a785786eae6268744826a8e04a52aee54b14
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-05 14:53:58 +0900

    setting: 초기 보안 설정 추가

---
+++ src/main/java/com/goorm/team9/icontact/config/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.goorm.team9.icontact.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger는 인증 없이 허용
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
