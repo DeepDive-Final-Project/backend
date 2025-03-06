eeff39ea103652563845d4c928d9be478a6bd47c
leeje0506 <lemonlab0408@gmail.com>
2025-03-06 11:24:33 +0900

    feat: enum 생성
    - Role, Career, Industry, Status, Provider 생성

---
+++ src/main/java/com/goorm/team9/icontact/sociallogin/domain/Provider.java
@@ -0,0 +1,8 @@
+package com.goorm.team9.icontact.sociallogin.domain;
+
+public enum Provider {
+    GOOGLE,
+    GITHUB,
+    KAKAO,
+    NAVER
+}
---
+++ src/main/java/com/goorm/team9/icontact/sociallogin/domain/Role.java
@@ -0,0 +1,8 @@
+package com.goorm.team9.icontact.sociallogin.domain;
+
+public enum Role {
+    STUDENT,
+    WORKER,
+    FREELANCER,
+    JOB_SEEKER
+}
