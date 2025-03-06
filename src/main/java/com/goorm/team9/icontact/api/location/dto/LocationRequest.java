07a1153f21a8831bc04bdcb16a83a4e14e66b299
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-03-06 13:37:14 +0900

    chore: LocationRequest 기본 생성자

---
+++ src/main/java/com/goorm/team9/icontact/api/location/dto/LocationRequest.java
@@ -0,0 +1,14 @@
+package com.goorm.team9.icontact.api.location.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationRequest {
+    private String userId;
+    private double latitude;
+    private double longitude;
+}
