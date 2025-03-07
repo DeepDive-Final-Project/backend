9bfe08d47c4cee9c73544f9f7db5ac9e9e08aaf3
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-07 13:05:56 +0900

    chore: Add ALB Health Check Config

--- src/main/java/com/goorm/team9/icontact/config/security/SecurityConfig.java
+++ src/main/java/com/goorm/team9/icontact/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,11 +16,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger는 인증 없이 허용
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**, /actuator/health").permitAll() // Swagger는 인증 없이 허용
                         .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults())
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
