df1112bdefd7f19c7c7c425987553cc47ebd3756
tjdgusdk <sha6639@naver.com>
2025-03-07 12:03:50 +0900

    setting: websocket 설정 추가

--- src/main/java/com/goorm/team9/icontact/config/websocket/WebSocketHandshakeInterceptor.java
+++ src/main/java/com/goorm/team9/icontact/config/websocket/WebSocketHandshakeInterceptor.java
@@ -1,8 +1,9 @@
 package com.goorm.team9.icontact.config.websocket;
 
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.Map;
@@ -12,6 +13,17 @@ public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
                                    WebSocketHandler wsHandler, Map<String, Object> attributes) {
+
+        String chatRoomId = request.getURI().getPath().split("/")[2];
+
+        String senderNickname = request.getURI().getQuery();
+        String[] params = senderNickname.split("=");
+        if (params.length == 2 && "senderNickname".equals(params[0])) {
+            senderNickname = params[1];
+        }
+
+        attributes.put("chatRoomId", chatRoomId);
+        attributes.put("senderNickname", senderNickname);
         return true;
     }
 
