4b23b92d8ded5c5593135ae1a919cb42c6e44e6e
leeje0506 <lemonlab0408@gmail.com>
2025-03-08 12:35:12 +0900

    feat: HomeController 수정
    - 권한 확인 설정 추가

--- src/main/java/com/goorm/team9/icontact/sociallogin/controller/HomeController.java
+++ src/main/java/com/goorm/team9/icontact/sociallogin/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.goorm.team9.icontact.sociallogin.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/home")
     public ResponseEntity<String> home() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("🔍 현재 사용자: " + authentication);
+
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 필요");
+        }
+
         return ResponseEntity.ok("OAuth 로그인 성공! 홈 화면입니다.");
     }
 }
