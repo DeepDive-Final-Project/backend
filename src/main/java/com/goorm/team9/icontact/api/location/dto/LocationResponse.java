c5cc5f6cad616387c8467d17939261c8240b12e3
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-03-10 10:48:10 +0900

    feat: LocationResponse DTO에 관심 분야 및 거리 필드 추가

--- src/main/java/com/goorm/team9/icontact/api/location/dto/LocationResponse.java
+++ src/main/java/com/goorm/team9/icontact/api/location/dto/LocationResponse.java
@@ -9,5 +9,20 @@ public class LocationResponse {
     private String userId;
     private double latitude;
     private double longitude;
-    private double distance;
+    private String distance;
+    private String interest;
+
+    public LocationResponse(String userId, double latitude, double longitude, Double distance, String interest) {
+        this.userId = userId;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+        if (distance != null) {
+            this.distance = String.format("%.1fm", distance);
+        } else {
+            this.distance = "0.0m";
+        }
+
+        this.interest = interest;
+    }
 }
