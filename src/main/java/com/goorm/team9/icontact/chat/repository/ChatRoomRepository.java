6af49aa6a67331867edd76d7c8d07a519cea2787
tjdgusdk <sha6639@naver.com>
2025-03-10 13:31:05 +0900

    feat: 채팅방 조회를 위한 findByParticipants 메서드 추가

--- src/main/java/com/goorm/team9/icontact/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/goorm/team9/icontact/chat/repository/ChatRoomRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findBySenderNicknameAndReceiverNickname(String senderNickname, String receiverNickname);
     List<ChatRoom> findBySenderNicknameOrReceiverNickname(String senderNickname, String receiverNickname);
 
     @Query("SELECT c FROM ChatRoom c WHERE " +
@@ -15,7 +17,6 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
             "(c.senderNickname = :receiverNickname AND c.receiverNickname = :senderNickname)")
 
     ChatRoom findByParticipants(@Param("senderNickname") String senderNickname,
-                                @Param("receiverNickname") String receiverNickname);
-
+    @Param("receiverNickname") String receiverNickname);
 
 }
