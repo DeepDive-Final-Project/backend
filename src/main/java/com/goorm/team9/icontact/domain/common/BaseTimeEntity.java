8d4ae496579d940f009333d05278eed6c52ac379
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-03-10 11:18:03 +0900

    feat: BaseTimeEntity 생성 및 생성·수정·삭제·차단 시간 필드

---
+++ src/main/java/com/goorm/team9/icontact/domain/common/BaseTimeEntity.java
@@ -0,0 +1,32 @@
+package com.goorm.team9.icontact.domain.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+
+    @Column
+    private LocalDateTime deleted_at;
+
+    @Column
+    private LocalDateTime blocked_at;
+
+}
