e0b07ce9b8e31be87808b9f102513961cb0044f2
tjdgusdk <sha6639@naver.com>
2025-03-11 16:08:45 +0900

    feat: 닉네임 기반 채팅방 조회

--- src/main/java/com/goorm/team9/icontact/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/goorm/team9/icontact/chat/repository/ChatRoomRepository.java
@@ -6,17 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    Optional<ChatRoom> findBySenderNicknameAndReceiverNickname(String senderNickname, String receiverNickname);
-    List<ChatRoom> findBySenderNicknameOrReceiverNickname(String senderNickname, String receiverNickname);
 
-    @Query("SELECT c FROM ChatRoom c WHERE " +
-            "(c.senderNickname = :senderNickname AND c.receiverNickname = :receiverNickname) OR " +
-            "(c.senderNickname = :receiverNickname AND c.receiverNickname = :senderNickname)")
-
-    ChatRoom findByParticipants(@Param("senderNickname") String senderNickname,
-    @Param("receiverNickname") String receiverNickname);
-
+    @Query("SELECT c FROM ChatRoom c WHERE c.senderNickname.nickName = :nickname OR c.receiverNickname.nickName = :nickname")
+    List<ChatRoom> findBySenderNicknameOrReceiverNickname(@Param("nickname") String nickname);
 }
