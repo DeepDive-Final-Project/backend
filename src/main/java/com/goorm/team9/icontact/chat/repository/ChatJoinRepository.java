a0d3bb6f8bc6b5fe6cbe53ce6ba89328d0e9d666
tjdgusdk <sha6639@naver.com>
2025-03-11 16:01:46 +0900

    feat: ChatJoinRepository 추가 및 채팅방 참여 관리 구현

---
+++ src/main/java/com/goorm/team9/icontact/chat/repository/ChatJoinRepository.java
@@ -0,0 +1,16 @@
+package com.goorm.team9.icontact.chat.repository;
+
+import com.goorm.team9.icontact.chat.entity.ChatJoin;
+import com.goorm.team9.icontact.chat.entity.ChatRoom;
+import com.goorm.team9.icontact.domain.client.entity.ClientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatJoinRepository extends JpaRepository<ChatJoin, Long> {
+
+    Optional<ChatJoin> findByChatRoomAndClientId(ChatRoom chatRoom, ClientEntity client);
+
+    List<ChatJoin> findByClientAndExitedFalse(ClientEntity client);
+}
