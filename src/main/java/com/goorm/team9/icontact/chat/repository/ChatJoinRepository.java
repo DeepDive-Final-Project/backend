1731cfa98eef00db8607febc50d9643bdcf196bf
tjdgusdk <sha6639@naver.com>
2025-03-12 13:46:42 +0900

    fix: 타입 불일치 오류 수정

--- src/main/java/com/goorm/team9/icontact/chat/repository/ChatJoinRepository.java
+++ src/main/java/com/goorm/team9/icontact/chat/repository/ChatJoinRepository.java
@@ -5,12 +5,8 @@ import com.goorm.team9.icontact.chat.entity.ChatRoom;
 import com.goorm.team9.icontact.domain.client.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ChatJoinRepository extends JpaRepository<ChatJoin, Long> {
-
-    Optional<ChatJoin> findByChatRoomAndClientId(ChatRoom chatRoom, ClientEntity client);
-
-    List<ChatJoin> findByClientAndExitedFalse(ClientEntity client);
+    Optional<ChatJoin> findByChatRoomAndClientId(ChatRoom chatRoom, Long clientId);
 }
