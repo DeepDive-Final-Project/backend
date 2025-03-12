1b7c16036e5bd333d5c645077ce5bd4f1d4ff450
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-12 13:37:57 +0900

    chore: Enum Class 분기화

--- src/main/java/com/goorm/team9/icontact/domain/client/enums/Career.java
+++ src/main/java/com/goorm/team9/icontact/domain/client/enums/Career.java
@@ -3,20 +3,31 @@ package com.goorm.team9.icontact.domain.client.enums;
 import com.goorm.team9.icontact.domain.common.EnumWithDescription;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum Career implements EnumWithDescription {
-    JUNIOR("주니어 개발자", "DEV"),
-    SENIOR("시니어 개발자", "DEV"),
-    MIDDLE("미들 개발자", "DEV"),
-    HIGH_SCHOOL_STUDENT("고등학생", "STUDENT"),
-    UNIVERSITY_STUDENT("대학생", "STUDENT"),
-    ECT("기타", "ECT" );
+    JUNIOR("주니어", 1),
+    MIDDLE("미들", 1),
+    SENIOR("시니어", 1),
+    JOB_SEEKER("취업준비생", 1),
+    HIGH_SCHOOL_STUDENT("고등학생", 2),
+    UNIVERSITY_STUDENT("대학생", 2),
+    GRADUATE_STUDENT("대학원생", 2),
+    ECT("기타", 3);
 
     private final String description;
-    private final String apiCode;
+    private final int apiCode;
 
-    Career(String description, String apiCode) {
+    Career(String description, int apiCode) {
         this.description = description;
         this.apiCode = apiCode;
+    }
+
+    public static Career fromDescription(String description) {
+        return Arrays.stream(values())
+                .filter(c -> c.description.equals(description))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Career description: " + description));
     }
 }
--- src/main/java/com/goorm/team9/icontact/domain/client/enums/Role.java
+++ src/main/java/com/goorm/team9/icontact/domain/client/enums/Role.java
@@ -3,16 +3,29 @@ package com.goorm.team9.icontact.domain.client.enums;
 import com.goorm.team9.icontact.domain.common.EnumWithDescription;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum Role implements EnumWithDescription {
 
-    STUDENT("학생"),
-    WORKER("직장인"),
-    ECT("기타");
+    PD("프로덕트 디자이너", 1),
+    PM("프로덕트 매니지먼트", 1),
+    DEV("개발자", 1),
+    STUDENT("학생", 2),
+    ETC("기타", 3);
 
     private final String description;
+    private final int apiCode;
 
-    Role(String description) {
+    Role(String description, int apiCode) {
         this.description = description;
+        this.apiCode = apiCode;
+    }
+
+    public static Role fromDescription(String description) {
+        return Arrays.stream(values())
+                .filter(c -> c.description.equals(description))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Role description: " + description));
     }
 }
