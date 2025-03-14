3322920f16ba93921a0a84594f27105ac7f5a098
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-14 09:14:50 +0900

    chore: 커스텀 에러 추가

--- src/main/java/com/goorm/team9/icontact/common/error/ClientErrorCode.java
+++ src/main/java/com/goorm/team9/icontact/common/error/ClientErrorCode.java
@@ -11,10 +11,11 @@ public enum ClientErrorCode implements ErrorCodeInterface {
     // Client 관련 커스텀 에러는 10XX 의 형식을 사용
     CLIENT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 1001, "해당 프로필을 찾을 수 없습니다."),
     STATUS_IS_PRIVATE(HttpStatus.FORBIDDEN.value(), 1002, "해당 프로필은 비공개 상태입니다."),
-    EXISTED_EMAIL(HttpStatus.CONFLICT.value(), 1003, "이미 존재하는 이메일입니다.");
+    EXISTED_EMAIL(HttpStatus.CONFLICT.value(), 1003, "이미 존재하는 이메일입니다."),
+    NO_CHAT_OPPORTUNITY(HttpStatus.BAD_REQUEST.value(), 1004, "더 이상 채팅방을 만들 수 없습니다."),
+    CHAT_OPPORTUNITY_FULL(HttpStatus.BAD_REQUEST.value(), 1005, "채팅 기회가 이미 최대치입니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
     private final String description;
-
 }
