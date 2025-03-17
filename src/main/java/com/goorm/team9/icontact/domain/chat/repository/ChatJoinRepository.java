31279ec044b5a60c87881027fcabb6c7813969a9
tjdgusdk <sha6639@naver.com>
2025-03-17 14:38:15 +0900

    feat: 차단된 사용자 필터링을 위한 참여자 목록 조회 기능 추가

--- src/main/java/com/goorm/team9/icontact/domain/chat/repository/ChatJoinRepository.java
+++ src/main/java/com/goorm/team9/icontact/domain/chat/repository/ChatJoinRepository.java
@@ -6,6 +6,7 @@ import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatJoinRepository extends JpaRepository<ChatJoin, Long> {
@@ -17,4 +18,7 @@ public interface ChatJoinRepository extends JpaRepository<ChatJoin, Long> {
             "FROM ChatJoin cj " +
             "WHERE cj.chatRoom = :chatRoom AND cj.exited = false")
     long countByChatRoomAndExitedFalse(@Param("chatRoom") ChatRoom chatRoom);
+
+    @Query("SELECT cj FROM ChatJoin cj WHERE cj.chatRoom = :chatRoom AND cj.exited = false")
+    List<ChatJoin> findByChatRoom(@Param("chatRoom") ChatRoom chatRoom);
 }
