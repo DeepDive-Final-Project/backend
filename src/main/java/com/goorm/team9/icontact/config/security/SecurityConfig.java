80233a69e794b8a6812d6536e6160bd33a7de6c2
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-17 17:17:11 +0900

    chore: import 오류 수정

--- src/main/java/com/goorm/team9/icontact/config/security/SecurityConfig.java
+++ src/main/java/com/goorm/team9/icontact/config/security/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
