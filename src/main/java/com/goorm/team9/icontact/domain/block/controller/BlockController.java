d281bf3892ca6fd8378668b3042bf66add9ff0a8
tjdgusdk <sha6639@naver.com>
2025-03-18 17:05:50 +0900

    refactor: 차단/차단해제 API에 DTO 적용

--- src/main/java/com/goorm/team9/icontact/domain/block/controller/BlockController.java
+++ src/main/java/com/goorm/team9/icontact/domain/block/controller/BlockController.java
@@ -1,14 +1,13 @@
 package com.goorm.team9.icontact.domain.block.controller;
 
+import com.goorm.team9.icontact.domain.block.dto.BlockRequestDto;
+import com.goorm.team9.icontact.domain.block.dto.UnblockRequesetDto;
 import com.goorm.team9.icontact.domain.block.service.BlockService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @Tag(name = "Block API", description = "차단 및 차단 해제 API 입니다.")
 @RestController
@@ -20,27 +19,15 @@ public class BlockController {
 
     @Operation(summary = "사용자 차단 API", description = "사용자를 차단합니다.")
     @PostMapping("/block")
-    public ResponseEntity<String> blockUser(
-            @RequestBody
-            @Schema(example = "{\"blockerNickname\": \"userA\", \"blockedNickname\": \"userB\"}")
-            Map<String, String> request) {
-        String blockerNickname = request.get("blockerNickname");
-        String blockedNickname = request.get("blockedNickname");
-
-        blockService.blockUser(blockerNickname, blockedNickname);
+    public ResponseEntity<String> blockUser(@RequestBody BlockRequestDto request) {
+        blockService.blockUser(request.getBlockerNickname(), request.getBlockedNickname());
         return ResponseEntity.ok("사용자가 차단되었습니다.");
     }
 
     @Operation(summary = "사용자 차단 해제 API", description = "사용자를 차단 해제합니다.")
     @PostMapping("/unblock")
-    public ResponseEntity<String> unblockUser(
-            @RequestBody
-            @Schema(example = "{\"blockerNickname\": \"userA\", \"blockedNickname\": \"userB\"}")
-            Map<String, String> request) {
-        String blockerNickname = request.get("blockerNickname");
-        String blockedNickname = request.get("blockedNickname");
-
-        blockService.unblockUser(blockerNickname, blockedNickname);
+    public ResponseEntity<String> unblockUser(@RequestBody UnblockRequesetDto requeset) {
+        blockService.unblockUser(requeset.getBlockerNickname(), requeset.getBlockedNickname());
         return ResponseEntity.ok("사용자 차단이 해제되었습니다.");
     }
 }
