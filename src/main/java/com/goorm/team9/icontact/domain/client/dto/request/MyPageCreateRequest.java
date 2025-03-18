b2b230e6c63c13d94c84dcae454f92ce0f0aea05
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-18 17:09:55 +0900

    feat: 요청 타입 변경에 따른 DTO클래스 생성 및 제거

---
+++ src/main/java/com/goorm/team9/icontact/domain/client/dto/request/MyPageCreateRequest.java
@@ -0,0 +1,65 @@
+package com.goorm.team9.icontact.domain.client.dto.request;
+
+import com.goorm.team9.icontact.domain.client.enums.Career;
+import com.goorm.team9.icontact.domain.client.enums.Framework;
+import com.goorm.team9.icontact.domain.client.enums.Interest;
+import com.goorm.team9.icontact.domain.client.enums.Language;
+import com.goorm.team9.icontact.domain.client.enums.Status;
+import com.goorm.team9.icontact.domain.client.enums.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(description = "마이페이지 생성 요청 DTO", example = "{\n" +
+        "  \"nickName\": \"Noah\",\n" +
+        "  \"email\": \"noah@gmail.com\",\n" +
+        "  \"role\": \"DEV\",\n" +
+        "  \"career\": \"JUNIOR\",\n" +
+        "  \"status\": \"PUBLIC\",\n" +
+        "  \"introduction\": \"안녕하세요!\",\n" +
+        "  \"link\": \"https://www.test.com\",\n" +
+        "  \"topic1\": \"AI_Machine_Learning\",\n" +
+        "  \"topic2\": \"Server_Development\",\n" +
+        "  \"topic3\": \"API_Development\",\n" +
+        "  \"language\": \"JAVA\",\n" +
+        "  \"framework\": \"SPRINGBOOT\"\n" +
+        "}")
+public class MyPageCreateRequest {
+    @Schema(example = "Noah")
+    private String nickName;
+
+    @Schema(example = "noah@gmail.com")
+    private String email;
+
+    @Schema(example = "DEV")
+    private Role role;
+
+    @Schema(example = "JUNIOR")
+    private Career career;
+
+    @Schema(example = "PUBLIC")
+    private Status status;
+
+    @Schema(example = "안녕하세요!")
+    private String introduction;
+
+    @Schema(example = "https://www.test.com")
+    private String link;
+
+    @Schema(example = "AI_Machine_Learning")
+    private Interest topic1;
+
+    @Schema(example = "Server_Development")
+    private Interest topic2;
+
+    @Schema(example = "API_Development")
+    private Interest topic3;
+
+    @Schema(example = "JAVA")
+    private Language language;
+
+    @Schema(example = "SPRINGBOOT")
+    private Framework framework;
+}
---
+++ src/main/java/com/goorm/team9/icontact/domain/client/dto/request/MyPageUpdateRequest.java
@@ -0,0 +1,57 @@
+package com.goorm.team9.icontact.domain.client.dto.request;
+
+import com.goorm.team9.icontact.domain.client.enums.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(description = "마이페이지 수정 요청 DTO", example = "{\n" +
+        "  \"nickName\": \"NoahUpdated\",\n" +
+        "  \"role\": \"DEV\",\n" +
+        "  \"career\": \"SENIOR\",\n" +
+        "  \"status\": \"PUBLIC\",\n" +
+        "  \"introduction\": \"업데이트된 소개입니다!\",\n" +
+        "  \"link\": \"https://updated.com\",\n" +
+        "  \"topic1\": \"Cloud_Computing\",\n" +
+        "  \"topic2\": \"Data_Science\",\n" +
+        "  \"topic3\": \"DevOps\",\n" +
+        "  \"language\": \"PYTHON\",\n" +
+        "  \"framework\": \"DJANGO\"\n" +
+        "}")
+public class MyPageUpdateRequest {
+    @Schema(example = "NoahUpdated")
+    private String nickName;
+
+    @Schema(example = "DEV")
+    private Role role;
+
+    @Schema(example = "SENIOR")
+    private Career career;
+
+    @Schema(example = "PUBLIC")
+    private Status status;
+
+    @Schema(example = "업데이트된 소개입니다!")
+    private String introduction;
+
+    @Schema(example = "https://updated.com")
+    private String link;
+
+    @Schema(example = "Cloud_Computing")
+    private Interest topic1;
+
+    @Schema(example = "Data_Science")
+    private Interest topic2;
+
+    @Schema(example = "DevOps")
+    private Interest topic3;
+
+    @Schema(example = "PYTHON")
+    private Language language;
+
+    @Schema(example = "DJANGO")
+    private Framework framework;
+}
+
