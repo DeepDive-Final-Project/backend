48fb4d0e10c8f39b812666bd9684a48f50c9b2f1
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-18 13:23:05 +0900

    chore: 스웨거 설정 파일 수정

--- src/main/java/com/goorm/team9/icontact/config/swagger/SwaggerConfig.java
+++ src/main/java/com/goorm/team9/icontact/config/swagger/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.goorm.team9.icontact.config.swagger;
 
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,8 +27,30 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Team-9 / I-Contact")
-                .description("9팀 I-Contact API 명세서")
-                .version("1.0.0");
+                .title("Team-9 / 👀 I-Contact")
+                .version("1.0.0")
+                .description(
+                        """
+                        ## 🚀 I-Contact API 명세서
+                        ### 🎨 디자이너
+                        이준, 황소희
+                        \n\n
+                        ### 🖥️ 프론트엔드
+                        안주현, 윤가은, 유지수
+                        \n\n
+                        ### 🌐 백엔드
+                        이지은, 이서원, 성현아, 이정훈
+                        \n\n
+                        ### 🔗 링크
+                        📌 [I-Contact-Web](https://www.i-contacts.link)
+                        \n\n
+                        📌 [GitHub](https://github.com/DeepDive-Final-Project)
+                        \n\n
+                        📌 [Notion](https://www.notion.so/I-Contact-Team-Project-1a47fd02709c8034a6f0f43be421f718)
+                        \n\n
+                        📌 [Jira](https://qweqwerty12321-1740141206278.atlassian.net/jira/software/projects/ICT/pages)
+                        """
+                );
     }
+
 }
