bdb4ea216a5c5ebb8f8aafd4fd0db3fde8b10e83
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-19 10:21:32 +0900

    feat: 강의 예외 코드 추가

--- src/main/java/com/goorm/team9/icontact/common/error/LectureErrorCode.java
+++ src/main/java/com/goorm/team9/icontact/common/error/LectureErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum LectureErrorCode implements ErrorCodeInterface {
 
-    CONFERENCE_NOT_FOUND(404, 2001, "해당 컨퍼런스를 찾을 수 없습니다.");
+    // Lecture 관련 커스텀 에러는 40XX 의 형식을 사용
+    LECTURE_NOT_FOUND(404, 4001, "해당 컨퍼런스를 찾을 수 없습니다."),
+    CONFERENCE_NOT_FOUND(404, 4002, "해당 컨퍼런스를 찾을 수 없습니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
