70ba185aabdac6da1d9a557ef43e32d106a3898f
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-03-19 17:48:58 +0900

    feat: 위치 데이터 저장 예외 처리 코드 추가 (ALREADY_SAVED_LOCATION, NO_SAVED_LOCATION)

--- src/main/java/com/goorm/team9/icontact/common/exception/GlobalExceptionErrorCode.java
+++ src/main/java/com/goorm/team9/icontact/common/exception/GlobalExceptionErrorCode.java
@@ -16,11 +16,11 @@ public enum GlobalExceptionErrorCode implements ErrorCodeInterface {
     DUPLICATE_CLIENT(HttpStatus.CONFLICT, "중복된 참가자의 정보가 존재합니다.", 40901),
     INVALID_LOCATION_DATA(HttpStatus.BAD_REQUEST, "유효하지 않은 위도/경도 값입니다. 다시 확인해 주세요.", 40002),
     INVALID_USER_ID(HttpStatus.BAD_REQUEST, "가입된 참가자가 아닙니다. 다시 확인해 주세요.", 40004),
-    LOCATION_NOT_UPDATED(HttpStatus.BAD_REQUEST, "위치 정보가 변경되지 않았습니다.", 40005),
     INVALID_INTEREST(HttpStatus.BAD_REQUEST, "지정한 관심분야와 참가자가 입력한 관심분야가 일치하지 않습니다. 다시 확인해 주세요.", 40006),
     UNKNOWN_INTEREST(HttpStatus.NOT_FOUND, "해당 관심분야 찾을 수 없습니다: %s", 40404),
     MISSING_INTEREST(HttpStatus.BAD_REQUEST, "해당 참가자는 선택한 관심분야가 없으므로 더 이상 진행이 어렵습니다. 관리자에게 문의하세요.", 40007),
-    LOCATION_UPDATED(HttpStatus.OK, "위치 데이터가 갱신되었습니다.", 20001);
+    ALREADY_SAVED_LOCATION(HttpStatus.BAD_REQUEST, "최초 위치데이터를 저장합니다. /refresh를 사용하세요.", 40008),
+    NO_SAVED_LOCATION(HttpStatus.BAD_REQUEST, "최초 위치 데이터가 존재하지 않습니다. 먼저 /save를 통해 위치데이터를 저장하세요.", 40009);
 
 
     private final HttpStatus httpStatus;
