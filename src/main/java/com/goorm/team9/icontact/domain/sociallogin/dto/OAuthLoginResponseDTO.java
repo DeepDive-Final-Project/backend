819832a210c8d6360b97d17c6b307d5f85401d17
leeje0506 <lemonlab0408@gmail.com>
2025-03-20 22:57:52 +0900

    feat: DTO 생성
    - OAuthLoginResponseDTO
    - LoginUserResponseDTO

---
+++ src/main/java/com/goorm/team9/icontact/domain/sociallogin/dto/OAuthLoginResponseDTO.java
@@ -0,0 +1,22 @@
+package com.goorm.team9.icontact.domain.sociallogin.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OAuthLoginResponseDTO {
+    private String email;
+    private String provider; // github, google, kakao
+    private String accessToken;
+    private String refreshToken;
+    private String role;
+
+    public OAuthLoginResponseDTO(String email, String provider, String accessToken, String refreshToken, String role) {
+        this.email = email;
+        this.provider = provider;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.role = role;
+    }
+}
