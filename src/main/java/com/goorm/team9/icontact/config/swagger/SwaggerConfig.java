46d664022e4dd808fe88315cf3e2abdda2aba129
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-24 16:29:10 +0900

    chore: Swagger가 현재 접속한 URL 기준으로 호출되게 변경

--- src/main/java/com/goorm/team9/icontact/config/swagger/SwaggerConfig.java
+++ src/main/java/com/goorm/team9/icontact/config/swagger/SwaggerConfig.java
@@ -3,26 +3,17 @@ package com.goorm.team9.icontact.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI() { // Swagger가 현재 접속한 URL 기준으로 호출되게 변경!!!
         return new OpenAPI()
                 .components(new Components())
-                .info(apiInfo())
-                .servers(List.of(
-                        new Server().url("http://localhost:8080").description("Local 개발 서버"),
-                        new Server().url("https://www.i-contacts.link").description("배포 서버"),
-                        new Server().url("http://3.34.165.63:8080").description("배포 서버 A"),
-                        new Server().url("http://43.201.245.222:8080").description("배포 서버 B")
-                ));
+                .info(apiInfo());
     }
 
     private Info apiInfo() {
