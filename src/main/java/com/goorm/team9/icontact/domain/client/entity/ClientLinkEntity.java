8e935663b9ff0b0cd087f643104509213af49825
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-03-28 20:29:05 +0900

    feat: link리스트를 담는 테이블 추가

---
+++ src/main/java/com/goorm/team9/icontact/domain/client/entity/ClientLinkEntity.java
@@ -0,0 +1,25 @@
+package com.goorm.team9.icontact.domain.client.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "client_link")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientLinkEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String link;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    private ClientEntity client;
+}
