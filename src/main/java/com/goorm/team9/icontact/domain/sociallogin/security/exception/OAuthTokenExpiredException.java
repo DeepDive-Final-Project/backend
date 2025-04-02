f1c8896b272e43a8f59d2eb804074cba77151419
LeeJeongHoon <142333228+DawnIsProblem@users.noreply.github.com>
2025-04-02 22:47:03 +0900

    refactor: 중복 및 미사용 클래스 제거

--- src/main/java/com/goorm/team9/icontact/domain/sociallogin/security/exception/OAuthTokenExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.goorm.team9.icontact.domain.sociallogin.security.exception;
-
-public class OAuthTokenExpiredException extends RuntimeException {
-    public OAuthTokenExpiredException(String message) {
-        super(message);
-    }
-}
