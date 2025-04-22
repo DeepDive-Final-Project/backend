1235f7982b028af516453d2048798e403d1d8b18
DawnIsProblem <qweqwerty12321@gmail.com>
2025-04-23 00:21:16 +0900

    feat: 닉네임 수정 요청 dto 추가

---
+++ src/main/java/com/goorm/team9/icontact/domain/client/dto/request/UpdateNicknameRequestDto.java
@@ -0,0 +1,12 @@
+package com.goorm.team9.icontact.domain.client.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateNicknameRequestDto {
+
+    private String nickName;
+
+}
