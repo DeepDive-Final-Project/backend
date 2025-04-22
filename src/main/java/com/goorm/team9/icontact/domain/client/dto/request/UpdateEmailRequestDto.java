4df0bf807f2f57e3d989be116340bd89daa2007f
DawnIsProblem <qweqwerty12321@gmail.com>
2025-04-23 00:22:08 +0900

    feat: 이메일 수정 요청 dto 추가

---
+++ src/main/java/com/goorm/team9/icontact/domain/client/dto/request/UpdateEmailRequestDto.java
@@ -0,0 +1,12 @@
+package com.goorm.team9.icontact.domain.client.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateEmailRequestDto {
+
+    private String email;
+
+}
